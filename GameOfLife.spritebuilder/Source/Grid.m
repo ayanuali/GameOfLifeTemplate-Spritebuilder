9c033ab31473765510c2535979e59d27fbaa84ee
ayan <stormfrog.ayan@gmail.com>
2014-07-14 23:44:30 +0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -7,7 +7,13 @@
 //
 
 #import "Grid.h"
+#import "Creature.h"
 
+
+
+//this are variables that cannot be changed
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 10;
 @implementation Grid
 
 @end
