9627443610a7c7b30f936be7badede238afeb6e9
ayan <stormfrog.ayan@gmail.com>
2014-07-20 00:50:32 +0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -9,11 +9,59 @@
 #import "Grid.h"
 #import "Creature.h"
 
-
-
 //this are variables that cannot be changed
 static const int GRID_ROWS = 8;
 static const int GRID_COLUMNS = 10;
-@implementation Grid
 
+@implementation Grid
+{
+    NSMutableArray *_gridArray;
+    float _cellWidth;
+    float _cellHeight;
+}
+
+-(void)onEnter
+{
+    [super onEnter];
+    
+    [self setupGrid];
+    
+    self.userInteractionEnabled = YES;
+}
+
+-(void)setupGrid
+{
+    // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
+    _cellWidth = self.contentSize.width/GRID_COLUMNS;
+    _cellHeight = self.contentSize.height/GRID_ROWS;
+    
+    float x = 0;
+    float y = 0;
+    
+    //initialize the array as blank NSMutableArray
+    _gridArray = [NSMutableArray array];
+    
+    //initialize Creatures
+    for(int i = 0; i < GRID_ROWS; i++)
+    {
+        _gridArray[i] = [NSMutableArray array];
+        x = 0;
+        
+        for (int j = 0; j < GRID_COLUMNS; i++)
+        {
+            Creature *creature = [[Creature alloc] initCreature];
+            creature.anchorPoint = ccp(0, 0);
+            creature.position = ccp(x,y);
+            [self addChild:creature];
+            
+            // this is shorthand to access an array inside an array
+            _gridArray[i][j] = creature;
+            
+            x+=_cellWidth;
+        }
+        
+        y+=_cellHeight;
+    }
+    
+}
 @end
