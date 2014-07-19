63e0b5d2443a3c4fad3df20947d6c8b2ea1f29b8
ayan <stormfrog.ayan@gmail.com>
2014-07-20 00:52:20 +0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -47,7 +47,7 @@ static const int GRID_COLUMNS = 10;
         _gridArray[i] = [NSMutableArray array];
         x = 0;
         
-        for (int j = 0; j < GRID_COLUMNS; i++)
+        for (int j = 0; j < GRID_COLUMNS; j++)
         {
             Creature *creature = [[Creature alloc] initCreature];
             creature.anchorPoint = ccp(0, 0);
