914f941c6ced4b927d73a29df1888b898f9c65db
ayan <stormfrog.ayan@gmail.com>
2014-07-20 00:59:08 +0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Creature.m
+++ GameOfLife.spritebuilder/Source/Creature.m
@@ -11,7 +11,7 @@
 @implementation Creature
 -(instancetype)initCreature{
     //since we made Creature inherit from CCSprite, 'super' below refers to CCSprite
-    self = [super initWithImageNamed:@"GameOfLifeAssets/Assets/resources-auto/bubble.png"];
+    self = [super initWithImageNamed:@"bubble.png"];
     
     if(self)
     {
