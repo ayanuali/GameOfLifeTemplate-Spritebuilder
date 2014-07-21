1d202621ce436c9b61994c8b43edad056f56fcf4
ayan <stormfrog.ayan@gmail.com>
2014-07-21 13:38:30 +0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -146,7 +146,7 @@ static const int GRID_COLUMNS = 10;
     
 }
 
--(BOOl)isIndexValidForX:(int)x andY:(int)y
+-(BOOL)isIndexValidForX:(int)x andY:(int)y
 {
     BOOL isIndexValid = YES;
     if(x < 0 || y < 0 || x >= GRID_ROWS || y >= GRID_COLUMNS)
